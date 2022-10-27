cc9f5111dd742b71d44357e2bac51c18a5a3e95d
SR-Pittu <102750581+SR-Pittu@users.noreply.github.com>
2022-10-26 19:56:17 -0500

    bonus commit

---
+++ routes/computation.js
@@ -0,0 +1,49 @@
+var express = require('express');
+
+var router = express.Router();
+
+
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+
+    var n1=Math.floor(Math.random()*10)
+
+    var n2=(Math.random())
+
+    var n3=Math.floor(Math.random()*10)
+
+    var froundFunction=Math.fround(n1);
+
+    var randomFunction;
+
+    var roundFunction=Math.round(n3)
+
+
+
+    res.render('computation',
+
+    {
+
+        title:'SobhaReddy Pittu Computes Functions',
+
+        n1:n1,
+
+        n2:n2,
+
+        n3:n3,
+
+        a:froundFunction,
+
+        b:randomFunction,
+
+        c:roundFunction,
+
+    });
+
+     
+
+});
+
+module.exports=router;
